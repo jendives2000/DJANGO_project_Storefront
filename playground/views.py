44722a86ab5082a6e02e1633e18853362e53240b
Jean-Yves TRAN <jendives2000@gmail.com>
2024-04-01 16:32:53 +0300

    Started with views

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# URL = 127.0.0.1:8000/playground/...
+
+# 1. Create a view here (a function that handles an HTTP request)
+# 2. then map it to the urls.py : this will make the function being called when the requested URL is actually requested.
