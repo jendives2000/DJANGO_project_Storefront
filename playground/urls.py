63d725eb1c9144aff1401f857ca9981d190e8353
Jean-Yves TRAN <jendives2000@gmail.com>
2024-04-01 16:38:34 +0300

    Created urls.py

---
+++ playground/urls.py
@@ -0,0 +1,5 @@
+# to get the primary URL chunks from the storefront (main project): 127.0.0.1:8000/
+from django.urls import path
+
+# to get the HTTP request handling functions from the playground app: .../playground/...
+from . import views
