b709fb28571046a198b0bcfc59fde4e0ba619e2d
Jean-Yves TRAN <jendives2000@gmail.com>
2024-04-02 15:32:47 +0300

    Declared class LikedItems

--- likes/models.py
+++ likes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class LikedItems(models.Model):
+    # 1toM relationship: parent: User (above import)
+    # one user can have many LikedItem
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # Generic relationship:
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
