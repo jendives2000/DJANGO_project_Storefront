890e6564136f4d3a57e913b27b191312484dfbf3
Jean-Yves TRAN <jendives2000@gmail.com>
2024-04-02 16:07:54 +0300

    Typos

--- store/models.py
+++ store/models.py
@@ -49,15 +49,11 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE
     )
+
     # this class enables us to customize further the customer DB
     class Meta:
-        db_table = 'store_customer'
-        indexes = [
-            models.Index(fields=['first_name', 'last_name])
-        ]
-
-        
-        
+        db_table = "store_customer"
+        indexes = [models.Index(fields=["last_name", "first_name"])]
 
 
 class Order(models.Model):
